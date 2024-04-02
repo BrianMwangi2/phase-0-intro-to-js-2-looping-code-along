b2f3f623c83c9c42ef63eacb81a4ed8ec8ddc002
Brian <brian.mwangi2@student.moringaschool.com>
2024-04-02 15:50:08 +0300

    adds files

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    let thankYouMessages = []; // Create an empty array to hold the messages
+  
+    // Iterate through the input array and build 'thank you' messages
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message); // Add the message to the array
+    }
+  
+    return thankYouMessages; // Return the array of messages
+  }
+  let names = ["Guadalupe", "Ollie", "Aki"];
+  let thankYouMessages = writeCards(names);
+  console.log(thankYouMessages);
+
+  // while loop 
+  function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  
+  // Example usage:
+  countDown(10);
+  
